fdc1d37ada58ba1cba36e8a45ba30b4120adb215
MyLastBrainCell <85825806+MyLastBrainCell@users.noreply.github.com>
2021-06-13 20:09:47 +1000

    Update enigma-encoder.js

--- enigma-encoder.js
+++ enigma-encoder.js
@@ -5,8 +5,6 @@ function getInputValue(){
     document.getElementById("encoded").innerHTML = code_out;
 }
 
-document.getElementById("encoded").innerHTML = 'test';
-
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k',
 'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
