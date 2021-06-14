7200c3aac600ba0f56a5253f61bea7a76bc58165
MyLastBrainCell <85825806+MyLastBrainCell@users.noreply.github.com>
2021-06-14 10:07:09 +1000

    Create Vernier-caliper.js

---
+++ Vernier-caliper.js
@@ -0,0 +1,12 @@
+function resetCaliper() {
+    document.getElementById("top-img").style.left = "0px";
+  };
+
+function getInputValue() {
+  let inputVal = document.getElementById("myInput").value;
+  document.getElementById("top-img").style.left=cmToPixels(inputVal)+"px";
+};
+
+function cmToPixels(cm) {
+  return Math.round(Number(cm) * (899/15));
+}
