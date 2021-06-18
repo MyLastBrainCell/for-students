42d134505908f5c47bbdd2dad0c3e81300322eb3
MyLastBrainCell <85825806+MyLastBrainCell@users.noreply.github.com>
2021-06-19 08:46:07 +1000

    Update circuit-practice.js

--- circuit-practice.js
+++ circuit-practice.js
@@ -1,39 +1,39 @@
 function getInputValue(){
     // Selecting the input element and get its value 
 
-    let V = document.getElementById("batteryVoltage").value;
+    let Voltage = document.getElementById("batteryVoltage").value;
     let RA = document.getElementById("resA").value;
     let RB = document.getElementById("resB").value;
     let RC = document.getElementById("resC").value;
     let circuitSwitch = document.getElementById("circuitSwitch").value;
     let lampMinCurrent = document.getElementById("lampMinCurrent").value;
     
-    document.getElementById("test") = String(V);
+    document.getElementById("test") = String(Voltage);
 
-    if (V.length === 0) {V = 12};
+    if (Voltage.length === 0) {Voltage = 12};
     if (RA.length === 0) {RA = 5};
     if (RB.length === 0) {RB = 8};
     if (RC.length === 0) {RC = 1};
     if (circuitSwitch !== 'open' && circuitSwitch !== 'closed') {xNameInput = 'open'};
     if (lampMinCurrent.length === 0) {yNameInput = 0.05};
 
-    let lampStates = GetLampState(V,RA,RB,circuitSwitch,LampMinCurrent);
+    let lampStates = GetLampState(Voltage,RA,RB,circuitSwitch,LampMinCurrent);
     let filePath = 's/' + imgMatcher(lampStates,circuitSwitch);
     document.getElementById("circuitImg").src = filePath;
 }
 
 function resetBox() {document.getElementById("circuitImg").src = "s/circuit-closed-left.png"}
 
-function GetLampState(V,RA,RB,circuitSwitch,lampMinCurrent) {
+function GetLampState(Voltage,RA,RB,circuitSwitch,lampMinCurrent) {
 
   if (circuitSwitch === 'open') {
-    let currentA = V / RA;
+    let currentA = Voltage / RA;
 
     if (currentA >= lampMinCurrent) { return [1,0,0]}
     else {return [0,0,0]};
   } else if (circuitSwitch === 'closed') {
-    let currentA = V / RA;
-    let currentB = V / RB;
+    let currentA = Voltage / RA;
+    let currentB = Voltage / RB;
 
     if (currentA >= lampMinCurrent && currentB >= lampMinCurrent) {return [1,1,0]}
     else if (currentA >= lampMinCurrent && currentB < lampMinCurrent) {return [1,0,0]}
