224278d2f1f914dfa1fe64dd83475df947a894ec
MyLastBrainCell <85825806+MyLastBrainCell@users.noreply.github.com>
2021-06-19 09:40:26 +1000

    Update circuit-practice.js

--- circuit-practice.js
+++ circuit-practice.js
@@ -24,7 +24,7 @@ function getInputValue(){
     let circuitSwitch = "open";
     let lampMinCurrent = 0.05;
 
-    let lampStates = GetLampState(Voltage,RA,RB,circuitSwitch,LampMinCurrent);
+    let lampStates = String(GetLampState(Voltage,RA,RB,circuitSwitch,LampMinCurrent));
     document.getElementById("lampState").innerHTML = String(lampStates);
     let filePath = 'https://hosting.photobucket.com/images/i/MyLastBrainCell/' + imgMatcher(lampStates,circuitSwitch);
     document.getElementById("test").innerHTML = String(filePath);
