cc61ab2e524bdd975efd710bfe1f4baddcf679cd
MyLastBrainCell <85825806+MyLastBrainCell@users.noreply.github.com>
2021-06-19 10:31:53 +1000

    Update circuit-practice.js

--- circuit-practice.js
+++ circuit-practice.js
@@ -1,26 +1,25 @@
 function getInputValue(){
-    // Selecting the input element and get its value 
+  // Selecting the input element and get its value 
 
-    let V = body.getElementById("batteryVoltage").value;
-    let RA = body.getElementById("resA").value;
-    let RB = body.getElementById("resB").value;
-    let RC = body.getElementById("resC").value;
-    //let circuitSwitch = body.getElementById("circuitSwitch").value;
-    //let lampMinCurrent = body.getElementById("lampMinCurrent").value;
-    let circuitSwitch = "closed";
-    let lampMinCurrent = 0.01;
+  const Voltage = document.getElementById("batteryVoltage").value;
+  const RA = document.getElementById("resA").value;
+  const RB = document.getElementById("resB").value;
+  const RC = document.getElementById("resC").value;
 
-    //if (V.length === 0) {V = 12};
-    //if (RA.length === 0) {RA = 5};
-    //if (RB.length === 0) {RB = 8};
-    //if (RC.length === 0) {RC = 1};
-    //if (circuitSwitch !== 'open' && circuitSwitch !== 'closed') {xNameInput = 'open'};
-    //if (lampMinCurrent.length === 0) {yNameInput = 0.05};
-    
-    body.getElementById("lampState") = String(V) + " , " + String(RA) + " , " + String(RB) + " , " + String(RC) + " , " + String(circuitSwitch) + " , " + String(lampMinCurrent);
-    
-    let lampStates = String(GetLampState(Voltage,RA,RB,circuitSwitch,LampMinCurrent));
-    let filePath = 'https://hosting.photobucket.com/images/i/MyLastBrainCell/' + imgMatcher(lampStates,circuitSwitch);
+  let circuitSwitch = document.getElementById("circuitSwitch").value;
+  let lampMinCurrent = document.getElementById("lampMinCurrent").value;
+
+  if (Voltage.length === 0) {Voltage = 12};
+  if (RA.length === 0) {RA = 5};
+  if (RB.length === 0) {RB = 8};
+  if (RC.length === 0) {RC = 1};
+  if (circuitSwitch !== 'open' && circuitSwitch !== 'closed') {xNameInput = 'open'};
+  if (lampMinCurrent.length === 0) {yNameInput = 0.05};
+
+  let lampStates = String(GetLampState(Voltage,RA,RB,circuitSwitch,lampMinCurrent));
+  document.getElementById("lampState").innerHTML = lampStates;
+  let filePath = 'https://hosting.photobucket.com/images/i/MyLastBrainCell/' + imgMatcher(lampStates,circuitSwitch);
+  document.getElementById("circuitImg").src = filePath;
 }
 
 function resetBox() {document.getElementById("circuitImg").src = "https://hosting.photobucket.com/images/i/MyLastBrainCell/circuit-open-none.png"}
@@ -28,29 +27,29 @@ function resetBox() {document.getElementById("circuitImg").src = "https://hostin
 
 function GetLampState(Voltage,RA,RB,circuitSwitch,lampMinCurrent) {
 
-  if (circuitSwitch === 'open') {
-    let currentA = Voltage / RA;
+if (circuitSwitch === 'open') {
+  let currentA = Voltage / RA;
 
-    if (currentA >= lampMinCurrent) { return [1,0,0]}
-    else {return [0,0,0]};
-  } else if (circuitSwitch === 'closed') {
-    let currentA = Voltage / RA;
-    let currentB = Voltage / RB;
+  if (currentA >= lampMinCurrent) { return "[1,0,0]"}
+  else {return "[0,0,0]"};
+} else if (circuitSwitch === 'closed') {
+  let currentA = Voltage / RA;
+  let currentB = Voltage / RB;
 
-    if (currentA >= lampMinCurrent && currentB >= lampMinCurrent) {return "[1,1,0]"}
-    else if (currentA >= lampMinCurrent && currentB < lampMinCurrent) {return "[1,0,0]"}
-    else if (currentA < lampMinCurrent && currentB >= lampMinCurrent) {return "[0,1,0]"}
-    else {return [0,0,0]};
-  }
+  if (currentA >= lampMinCurrent && currentB >= lampMinCurrent) {return "[1,1,0]"}
+  else if (currentA >= lampMinCurrent && currentB < lampMinCurrent) {return "[1,0,0]"}
+  else if (currentA < lampMinCurrent && currentB >= lampMinCurrent) {return "[0,1,0]"}
+  else {return "[0,0,0]"};
+}
 }
 
 function imgMatcher(lampStates,circuitSwitch) {
 
-  if (lampStates === "[0,0,0]" && circuitSwitch === 'open') {return "circuit-open-none.png"}
-  else if (lampStates === "[0,0,0]" && circuitSwitch === 'closed') {return "circuit-closed-0.png"}
-  else if (lampStates === "[1,0,0]" && circuitSwitch === 'closed') {return "circuit-closed-left.png"}
-  else if (lampStates === "[1,0,0]" && circuitSwitch === 'open') {return "circuit-open-left.png"}
-  else if (lampStates === "[0,1,0]" && circuitSwitch === 'closed') {return "circuit-closed-middle.png"}
-  else if (lampStates === "[1,1,0]" && circuitSwitch === 'closed') {return "circuit-closed-2.png"}
+if (lampStates === "[0,0,0]" && circuitSwitch === 'open') {return "circuit-open-none(1).png"}
+else if (lampStates === "[0,0,0]" && circuitSwitch === 'closed') {return "circuit-closed-0(1).png"}
+else if (lampStates === "[1,0,0]" && circuitSwitch === 'closed') {return "circuit-closed-left(1).png"}
+else if (lampStates === "[1,0,0]" && circuitSwitch === 'open') {return "circuit-open-left(1).png"}
+else if (lampStates === "[0,1,0]" && circuitSwitch === 'closed') {return "circuit-closed-middle(1).png"}
+else if (lampStates === "[1,1,0]" && circuitSwitch === 'closed') {return "circuit-closed-2(1).png"}
 }
 
