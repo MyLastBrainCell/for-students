13bb89609ed24508c0e7f6aa67aca789a77791df
MyLastBrainCell <85825806+MyLastBrainCell@users.noreply.github.com>
2021-06-23 11:20:53 +1000

    Update Vernier-caliper.js

--- Vernier-caliper.js
+++ Vernier-caliper.js
@@ -1,9 +1,7 @@
 function getInputValue() {
-  document.getElementById("measureReadout").innerHTML = "Measurements = ugh mm";
-  
-  let inputVal = document.getElementById("myInput").value;
-  document.getElementById("top-img").style.left=inputVal+"px";
-  //document.getElementById("measureReadout").innerHTML = "Measurement = " + pixToDist(inputVal) + "mm";
+  let inputVal = document.getElementById('myInput').value;
+  document.getElementById('top-img').style.left=inputVal+'px';
+  document.getElementById('measureReadout').innerHTML = 'Measurement = ' + pixToDist(inputVal) + 'mm';
 };
 
 function pixToDist(px) {
