ec66b71cba50932b6f1dca077f0bb0501975256f
MyLastBrainCell <85825806+MyLastBrainCell@users.noreply.github.com>
2021-06-25 12:09:54 +1000

    Update Vernier-caliper.js

--- Vernier-caliper.js
+++ Vernier-caliper.js
@@ -7,5 +7,6 @@ function getInputValue() {
 function pixToDist(px) {
   // returns mm
   px = Number(px) - 80;
-  return Math.round( (Number(px)) * (1600/903))/10;
+  //return Math.round( (Number(px)) * (1600/903))/10;
+  return Math.round(Number(px));
 }
